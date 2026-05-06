01e9cc60222b86005cb16eb4897851ba3d96b45b
João Miguel Francisco de Souza <joanomiguel@gmail.com>
2026-05-06 14:24:23 -0300

    feat: configuraçcao inicial do projeto springboot

---
+++ src/test/java/com/backend/java/saude_e_bem_estar/SaudeEBemEstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.java.saude_e_bem_estar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeEBemEstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
