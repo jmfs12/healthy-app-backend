9263078e0b8f62e8e0931ff277e2abff312ab73a
iclasTH <iclasinprogramacao@gmail.com>
2026-06-10 17:08:36 -0300

    fix(AgendamentoDTO): Corrigindo erros de convenção nos DTOs

--- src/main/java/com/backend/java/saude_e_bem_estar/dto/AgendamentoRequestDTO.java
+++ src/main/java/com/backend/java/saude_e_bem_estar/dto/AgendamentoRequestDTO.java
@@ -6,22 +6,23 @@ import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 
 public record AgendamentoRequestDTO
 (   
-    @NotBlank(message = "O usuário que será consultado é obrigatório")
-    Long id_usuario,
+    @NotNull(message = "O usuário que será consultado é obrigatório")
+    Long idUsuario,
 
-    @NotBlank(message = "A unidade de Saúde onde é obrigatória")
-    Long id_unidade,
+    @NotNull(message = "A unidade de Saúde onde é obrigatória")
+    Long idUnidade,
 
     @NotBlank(message = "É necessário informar o tipo de servico")
-    String tipo_servico,
+    String tipoServico,
 
     @FutureOrPresent(message = "Não é possivel realizar um atendimento para um dia anterior")
-    @NotBlank(message = "É necessário informar a data e a hora do agendamento")
-    LocalDateTime dataHora_agendada
+    @NotNull(message = "É necessário informar a data e a hora do agendamento")
+    LocalDateTime dataHoraAgendada
 
    
 
--- src/main/java/com/backend/java/saude_e_bem_estar/dto/AgendamentoResponseDTO.java
+++ src/main/java/com/backend/java/saude_e_bem_estar/dto/AgendamentoResponseDTO.java
@@ -3,7 +3,8 @@ package com.backend.java.saude_e_bem_estar.dto;
 import java.time.LocalDateTime;
 
 public record AgendamentoResponseDTO(
+    Long idAgendamento,
     String status,
-    String tipo_servico,
-    LocalDateTime dataHora_agendada
+    String tipoServico,
+    LocalDateTime dataHoraAgendada
 ) {}
