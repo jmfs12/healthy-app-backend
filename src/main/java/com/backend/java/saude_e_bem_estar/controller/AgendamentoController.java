99f5b5907b6acab15a28d80014f288d4746b7f4d
iclasTH <iclasinprogramacao@gmail.com>
2026-06-12 02:21:55 -0300

    feat(AgendamentoController): Criando os métodos get, put e delete
    Ao adicionar dois métodos get, facilita o usuário buscar o seu agendamento pelo id ou pela data. Foi possivel adicionar os end-points de put e delete do tipo void, retornando 204 (No Content)

--- src/main/java/com/backend/java/saude_e_bem_estar/controller/AgendamentoController.java
+++ src/main/java/com/backend/java/saude_e_bem_estar/controller/AgendamentoController.java
@@ -4,16 +4,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.java.saude_e_bem_estar.dto.AgendamentoRequestDTO;
 import com.backend.java.saude_e_bem_estar.dto.AgendamentoResponseDTO;
-import com.backend.java.saude_e_bem_estar.dto.UsuarioResponseDTO;
+
 import com.backend.java.saude_e_bem_estar.entities.Agendamento;
-import com.backend.java.saude_e_bem_estar.entities.Usuario;
 import com.backend.java.saude_e_bem_estar.service.AgendamentoService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 
 
@@ -36,8 +44,40 @@ public class AgendamentoController {
         novoAgendamento.setTipo_servico(request.tipoServico());
         novoAgendamento.setDataHora_agendada(request.dataHoraAgendada());
         
-        Agendamento AgendamentoCriado = agendamentoService.criar(novoAgendamento);
-        return ResponseEntity.status(HttpStatus.CREATED).body(AgendamentoResponseDTO.fromEntity(AgendamentoCriado));
+        Agendamento agendamentoCriado = agendamentoService.criar(novoAgendamento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(AgendamentoResponseDTO.fromEntity(agendamentoCriado));
     }
+
+    @GetMapping("/{id_agendamento}")
+    public ResponseEntity<AgendamentoResponseDTO> getById(@PathVariable Long id_agendamento) {
+        
+        Agendamento agendamentoBuscado = agendamentoService.buscarPorId(id_agendamento);
+        return ResponseEntity.status(HttpStatus.OK).body(AgendamentoResponseDTO.fromEntity(agendamentoBuscado));
+    }
+
+    @GetMapping("/{dataHoraAgendamento}")
+    public ResponseEntity<AgendamentoResponseDTO> getByDataHora(@PathVariable LocalDateTime dataHoraAgendamento) {
+        
+        Agendamento agendamentoBuscado = agendamentoService.buscarPorData(dataHoraAgendamento);
+        return ResponseEntity.status(HttpStatus.OK).body(AgendamentoResponseDTO.fromEntity(agendamentoBuscado));
+    }
+    
+    @PutMapping("atualizar/{id_agendamento}")
+    public ResponseEntity<Void> putAgendamento(@PathVariable Long id_agendamento, @RequestBody Agendamento novosDados) {
+        
+        agendamentoService.atualizar(id_agendamento, novosDados);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("deletar/{id_agendamento}")
+    public ResponseEntity<Void> deleteAgendamento(@PathVariable Long id_agendamento) {
+        
+        agendamentoService.deletar(id_agendamento);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
     
 }
